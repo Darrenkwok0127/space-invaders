8435d2cdb469f14701f95a93d8c57e809cc5bd5a
Darrenkwok <darrenkwok0127@gmail.com>
2023-11-23 21:31:14 +0800

    spawn invader 2

--- Assets/Scripts/invaders.cs
+++ Assets/Scripts/invaders.cs
@@ -4,7 +4,11 @@ public class invaders : MonoBehaviour
 {
     public int rows = 4;
     public int cols = 4;
+    public float step = 1.5f;
     public invader[] prefabs;
+
+    private Vector3 _direction = Vector3.left;
+    public float speed = 1f;
 
     private void Awake()
     {
@@ -15,13 +19,32 @@ public class invaders : MonoBehaviour
             {
                 float positionX = col * 1.5f;
                 Vector3 position = new Vector3(positionX, positionY);
-                invader invader = Instantiate(prefabs[0+row], this.transform);
+                invader invader = Instantiate(prefabs[row % prefabs.Length], this.transform);
                 invader.transform.position = position;
                 
             }
            
         }
         float X = cols * 1.5f / 2;
-        this.transform.position = new Vector3(- X + 0.75f, this.transform.position.y);
+        this.transform.position = new Vector3(- X + step /2, this.transform.position.y);
+    }
+    private void Update()
+    {
+        this.transform.position += _direction * speed * Time.deltaTime;
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        foreach(Transform invader in this.transform)
+        {
+            if (_direction == Vector3.right && invader.position.x >= rightEdge.x - 1f)
+            {
+                _direction = Vector3.left;
+            }   
+            else if (_direction == Vector3.left && invader.position.x <= leftEdge.x + 1f)
+            {
+                _direction = Vector3.right;
+            }
+
+        }
     }
 }

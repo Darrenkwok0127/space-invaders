325a31955c2ffbaa950936fcd91986863378e241
Darrenkwok <darrenkwok0127@gmail.com>
2023-11-23 20:54:26 +0800

    spawn invaders

--- Assets/Scripts/invaders.cs
+++ Assets/Scripts/invaders.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class invaders : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int rows = 4;
+    public int cols = 4;
+    public invader[] prefabs;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        for (int row = 0; row < rows; row++)
+        {
+            float positionY = row * 1.5f;
+            for (int col = 0; col < cols; col++)
+            {
+                float positionX = col * 1.5f;
+                Vector3 position = new Vector3(positionX, positionY);
+                invader invader = Instantiate(prefabs[0+row], this.transform);
+                invader.transform.position = position;
+                
+            }
+           
+        }
+        float X = cols * 1.5f / 2;
+        this.transform.position = new Vector3(- X + 0.75f, this.transform.position.y);
     }
 }
